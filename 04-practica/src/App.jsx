ff76b4a0b74afe0e1b5f9cde38551ccf675b1536
Juanmapp <juanma1694@gmail.com>
2023-08-03 17:33:30 -0300

    Practicando ejercicios de la clase 4

---
+++ 04-practica/src/App.jsx
@@ -0,0 +1,19 @@
+
+import "./app.css"
+import Producto from "./Producto";
+
+
+
+
+
+function App() {
+  return (
+    <div>
+      <h1>Produtos</h1>
+      <Producto />
+      
+    </div>
+  );
+}
+
+export default App;
